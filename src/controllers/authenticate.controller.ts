127cb2c40486b322d0691bc151f03ecc5bb12f31
Dionatan <dionatan.1751@gmail.com>
2023-09-15 16:26:06 -0300

    setup prisma test

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -4,22 +4,22 @@ import {
   UsePipes,
   Body,
   UnauthorizedException,
-} from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { compare } from "bcryptjs";
-import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
 
-import { PrismaService } from "src/prisma/prisma.service";
-import { z } from "zod";
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
 const authenticateBodyBodySchema = z.object({
   email: z.string().email(),
   password: z.string(),
-});
+})
 
-type AuthenticateBodyBodySchema = z.infer<typeof authenticateBodyBodySchema>;
+type AuthenticateBodyBodySchema = z.infer<typeof authenticateBodyBodySchema>
 
-@Controller("/sessions")
+@Controller('/sessions')
 export class AuthenticateController {
   constructor(
     private prisma: PrismaService,
@@ -29,26 +29,26 @@ export class AuthenticateController {
   @Post()
   @UsePipes(new ZodValidationPipe(authenticateBodyBodySchema))
   async handle(@Body() body: AuthenticateBodyBodySchema) {
-    const { email, password } = body;
+    const { email, password } = body
 
     const user = await this.prisma.user.findUnique({
       where: {
         email,
       },
-    });
+    })
 
     if (!user) {
-      throw new UnauthorizedException("User credentials do not match.");
+      throw new UnauthorizedException('User credentials do not match.')
     }
 
-    const isPasswordValid = await compare(password, user.password);
+    const isPasswordValid = await compare(password, user.password)
 
     if (!isPasswordValid) {
-      throw new UnauthorizedException("User credentials do not match.");
+      throw new UnauthorizedException('User credentials do not match.')
     }
 
-    const accessToken = this.jwt.sign({ sub: user.id });
+    const accessToken = this.jwt.sign({ sub: user.id })
 
-    return { access_token: accessToken };
+    return { access_token: accessToken }
   }
 }
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,22 +1,22 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { CurrentUser } from "src/auth/current-user.decorator";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { UserPayload } from "src/auth/jwt.strategy";
-import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user.decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
 
-import { PrismaService } from "src/prisma/prisma.service";
-import { z } from "zod";
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
-});
+})
 
-const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
-type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
-@Controller("/questions")
+@Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
@@ -26,10 +26,10 @@ export class CreateQuestionController {
     @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
     @CurrentUser() user: UserPayload,
   ) {
-    const { title, content } = body;
-    const userId = user.sub;
+    const { title, content } = body
+    const userId = user.sub
 
-    const slug = this.convertToSlug(title);
+    const slug = this.convertToSlug(title)
 
     await this.prisma.question.create({
       data: {
@@ -38,15 +38,15 @@ export class CreateQuestionController {
         content,
         slug,
       },
-    });
+    })
   }
 
   private convertToSlug(title: string): string {
     return title
       .toLocaleLowerCase()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .replace(/[^\w\s-]/g, "")
-      .replace(/\s+/g, "-");
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/[^\w\s-]/g, '')
+      .replace(/\s+/g, '-')
   }
 }
